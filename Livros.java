556bd019a1d45763a1eba3471b36879342586eb7
anacarolinesp <84101428+anacarolinesp@users.noreply.github.com>
2021-05-30 17:11:22 -0300

    Add files via upload

---
+++ Livros.java
@@ -0,0 +1,40 @@
+package br.edu.livros;
+
+import java.io.Serializable;
+
+public class Livros implements Serializable {
+ private String titulo; 
+ private Integer ano;
+ private String autor;
+ 
+ public Livros() {}
+	 public String getTitulo() {
+		 return titulo;
+		 
+	 }
+	 public void setTitulo(String titulo) {
+		 this.titulo = titulo;
+	 }
+	 public Integer getAno() {
+		 return ano;
+}
+	 public void setAno(Integer Ano) {
+		 this.ano = Ano;
+	 }
+
+	 public String getAutor() {
+		return autor;
+		}
+
+		public void setAutor(String autor) {
+			this.autor = autor;
+		
+		}
+		
+		public String toString() {
+			
+			return "Título: " + titulo + ", ano: " + ano + ", autora: " + autor + "." + "<br>";
+		}
+
+	}
+	
